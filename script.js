13cbca0db5a56c785c0025c5031abafc892f7b98
antoniogui33 <156611747+antoniogui33@users.noreply.github.com>
2024-03-28 13:06:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const myString = "joooj"
+
+const myNumber = 89
+
+const myBoolean = true 
+
+const myArray = [myString, myNumber, myBoolean]
+
+const myObject = {
+    myBoolean: false, 
+    myNumber: 88,
+    myString: "jaaaj",
+}
